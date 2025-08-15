73e3b9677c9784f692493ca2110f066891a9dc6d
Matias Lenci <matiaslenci@gmail.com>
2025-08-15 11:41:38 -0300

    add navbar

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,3 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+export const client = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
