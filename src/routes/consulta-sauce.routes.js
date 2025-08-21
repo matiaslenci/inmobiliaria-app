5122d1b5434970a5a547f72e7aa5e95a140ee013
Matias Lenci <matiaslenci@gmail.com>
2025-08-20 21:32:22 -0300

    consulta sauce

--- src/routes/consulta-sauce.routes.js
+++ src/routes/consulta-sauce.routes.js
@@ -1,23 +1,12 @@
 import express from "express";
 import {
-  obtenerMontoAgua,
-  obtenerMontoTasas,
-} from "../services/consulta.service.js";
+  renderConsultaSauce,
+  procesarConsulta,
+} from "../controllers/consulta-sauce.controller.js";
 
 const router = express.Router();
 
-router.get("/consulta-cuenta/sauce", (req, res) => {
-  res.render("consulta-sauce", { monto: null, error: null });
-});
-
-router.post("/consulta-cuenta/sauce", async (req, res) => {
-  const { nroCuenta } = req.body;
-  try {
-    const monto = await obtenerMontoAgua(nroCuenta); // O usa obtenerMontoTasas según corresponda
-    res.render("consulta-sauce", { monto, error: null });
-  } catch (error) {
-    res.render("consulta-sauce", { monto: null, error: error.message });
-  }
-});
+router.get("/consulta-sauce", renderConsultaSauce);
+router.post("/consulta-sauce", procesarConsulta);
 
 export default router;
