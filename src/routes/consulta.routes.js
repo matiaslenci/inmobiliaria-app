10fff137856147d0ebe0ef74056a812fb6026475
Matias Lenci <matiaslenci@gmail.com>
2025-08-29 12:54:51 -0300

    Añade archivos de configuración SEO, robots.txt y sitemap.xml; mejora la presentación y metadatos en la vista principal.

--- src/routes/consulta.routes.js
+++ src/routes/consulta.routes.js
@@ -7,6 +7,11 @@ import {
   descargarResultados,
 } from "../controllers/consulta.controller.js";
 import { authRequired } from "../middlewares/auth-guard.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = Router();
 
@@ -15,5 +20,16 @@ router.post("/upload", authRequired, uploadExcelMiddleware, procesarConsulta);
 
 router.get("/plantilla", descargarPlantilla);
 router.post("/exportar-resultados", descargarResultados);
+
+// SEO Routes
+router.get("/robots.txt", (req, res) => {
+  res.type("text/plain");
+  res.sendFile(path.join(__dirname, "../../public/robots.txt"));
+});
+
+router.get("/sitemap.xml", (req, res) => {
+  res.type("application/xml");
+  res.sendFile(path.join(__dirname, "../../public/sitemap.xml"));
+});
 
 export default router;
